73cc88f34807e424c085391b78c2ca7b4bd36e9a
Linda-Njau <lindanjau21@gmail.com>
2023-03-02 03:05:02 -0800

    height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height
+ *@tree: pointer to root
+ *Return: return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+	return (right_height + 1);
+}
