2242e6cb16f962369a5ba83e2ebb2e86553a9e11
Linda-Njau <lindanjau21@gmail.com>
2023-03-02 05:29:09 -0800

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_tree.h"
+/**
+ * binary_tree_size  - measure size of binary tree
+ * @tree: pointer to root node
+ * Return: zero or size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
+}
